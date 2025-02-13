b7f909942e6183ab0eed526ab9dffd79b36d9a2d
Diego Ocegueda <diego.ogg@hotmail.com>
2025-02-13 11:05:15 -0600

    correxion en const letra m

--- backend/utils/const.js
+++ backend/utils/const.js
@@ -8,7 +8,7 @@ export const allowedOrigins = [
   "http://ogg.deltako.com",
   "http://www.ogg.deltako.com",
   "https://ogg.deltako.com",
-  "https://www.mogg.deltako.com",
+  "https://www.ogg.deltako.com",
   "http://localhost:3000",
   "https://localhost:3000",
 ];
